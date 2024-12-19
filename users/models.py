50cdaa42af76846ebf89b9f3a31384fba71b8603
enki-somer <enkisomer@gmail.com>
2024-12-19 10:35:30 +0300

    Add custom user model with role-based access

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLES = [
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+        ('parent', 'Parent'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLES, default='student')
